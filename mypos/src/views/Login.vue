723b89d331ca2175dbdf67cce74ad5367b9f42fc
codemobiles <chaiyasit.t@gmail.com>
2020-11-23 15:35:45 +0700

    2134

--- mypos/src/views/Login.vue
+++ mypos/src/views/Login.vue
@@ -1,19 +1,37 @@
 <template>
-  <v-row align="center" justify="space-around">
-    <v-btn depressed>
-      Normal
-    </v-btn>
-    <v-btn depressed color="primary">
-      Primary
-    </v-btn>
-    <v-btn depressed color="error">
-      Error
-    </v-btn>
-    <v-btn depressed disabled>
-      Disabled
-    </v-btn>
-  </v-row>
+  <v-container>
+    <v-card class="mx-auto" max-width="400">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="@/assets/login_header.jpg"
+      >
+        <v-card-title>Top 10 Australian beaches</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pb-0">
+        Number 10
+      </v-card-subtitle>
+
+      <v-card-text class="text--primary">
+        <div>Whitehaven Beach</div>
+
+        <div>Whitsunday Island, Whitsunday Islands</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="orange" text>
+          Share
+        </v-btn>
+
+        <v-btn color="orange" text>
+          Explore
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
+
 <script>
 export default {
   name: "login",
