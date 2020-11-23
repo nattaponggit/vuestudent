d8c1a134266cc1ed12c6073939b0a930cdcfa754
codemobiles <chaiyasit.t@gmail.com>
2020-11-23 15:14:52 +0700

    2134

--- mypos/src/views/Login.vue
+++ mypos/src/views/Login.vue
@@ -1,7 +1,19 @@
 <template>
-  <div><h1>Login</h1></div>
+  <v-row align="center" justify="space-around">
+    <v-btn depressed>
+      Normal
+    </v-btn>
+    <v-btn depressed color="primary">
+      Primary
+    </v-btn>
+    <v-btn depressed color="error">
+      Error
+    </v-btn>
+    <v-btn depressed disabled>
+      Disabled
+    </v-btn>
+  </v-row>
 </template>
-
 <script>
 export default {
   name: "login",
