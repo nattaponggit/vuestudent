5daf089c78522b6aba45514d55253d7b4dcc0f69
codemobiles <chaiyasit.t@gmail.com>
2020-11-23 14:47:49 +0700

    2134

--- mypos/src/views/Register.vue
+++ mypos/src/views/Register.vue
@@ -1,5 +1,12 @@
 <template>
-  <div><h1>Register</h1></div>
+  <div>
+    <h1>Register</h1>
+    <form>
+      <input type="text" name="username" placeholder="Username" id="" />
+      <input type="text" name="password" placeholder="Password" id="" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
