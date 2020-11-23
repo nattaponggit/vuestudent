b1bc917d70006d3e2741a0b11970f7eec714e402
codemobiles <chaiyasit.t@gmail.com>
2020-11-23 13:54:51 +0700

    2134

--- mypos/src/router/index.js
+++ mypos/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
 Vue.use(VueRouter);
 
@@ -14,7 +14,7 @@ const routes = [
   {
     path: "/register",
     name: "register",
-    component: Login,
+    component: Register,
   },
 ];
 
