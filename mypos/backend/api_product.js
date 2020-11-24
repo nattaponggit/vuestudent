2ba66c1f30c3f239c3dbc757c7fecc0500b7414d
codemobiles <chaiyasit.t@gmail.com>
2020-11-24 15:06:46 +0700

    23

--- mypos/backend/api_product.js
+++ mypos/backend/api_product.js
@@ -2,14 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 // http://localhost:8081/api/v2/product
-router.get(
-  "/product",
-  (req, res, next) => {
+
+const i1 = (req, res, next) => {
+  if (req.query.token1 == "1234") {
     next();
-  },
-  (req, res) => {
-    res.json({ result: "product successfully" });
+  } else {
+    res.end("Invalid token1");
   }
-);
+};
+
+const i2 = (req, res, next) => {
+  if (req.query.token2 == "4321") {
+    next();
+  } else {
+    res.end("Invalid token2");
+  }
+};
+
+router.get("/product", i1, i2, (req, res) => {
+  res.json({ result: "product successfully" });
+});
 
 module.exports = router;
