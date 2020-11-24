b74f1571c442c589304e5d83225c8a2cb968ebfb
codemobiles <chaiyasit.t@gmail.com>
2020-11-24 15:53:58 +0700

    23

--- mypos/backend/api_product.js
+++ mypos/backend/api_product.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const jwt = require("./jwt")
 // destructure
 const {i1, i2} = require("./myinterceptors")
 // http://localhost:8081/api/v2/product
 // http://localhost:8081/api/v2/product?token1=1234&token2=4321
 
-router.get("/product", i1, i2, (req, res) => {
+router.get("/product", jwt.verify, (req, res) => {
   res.json({ result: "product successfully" });
 });
 
