3c93890a184654abe92ba9feca454eef29bb88b2
codemobiles <chaiyasit.t@gmail.com>
2020-11-24 15:11:52 +0700

    23

--- mypos/backend/myinterceptors.js
+++ mypos/backend/myinterceptors.js
@@ -1,16 +1,20 @@
 const i1 = (req, res, next) => {
-    if (req.query.token1 == "1234") {
-      next();
-    } else {
-      res.end("Invalid token1");
-    }
-  };
-  
-  const i2 = (req, res, next) => {
-    if (req.query.token2 == "4321") {
-      next();
-    } else {
-      res.end("Invalid token2");
-    }
-  };
-  
+  if (req.query.token1 == "1234") {
+    next();
+  } else {
+    res.end("Invalid token1");
+  }
+};
+
+const i2 = (req, res, next) => {
+  if (req.query.token2 == "4321") {
+    next();
+  } else {
+    res.end("Invalid token2");
+  }
+};
+
+module.exports = {
+  i1,
+  i2,
+};
