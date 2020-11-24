3d65087aae8fa0163e8e81c8d2ac0b65cb14d94d
codemobiles <chaiyasit.t@gmail.com>
2020-11-24 11:46:39 +0700

    23

--- mypos/src/views/Register.vue
+++ mypos/src/views/Register.vue
@@ -1,29 +1,67 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <form @submit.prevent="submit">
-      <input type="text" v-model="account.username" placeholder="Username" />
-      <input type="text" v-model="account.password" placeholder="Password" />
-      <button type="submit">Submit</button>
-      <span>#Debug {{ account }}</span>
-    </form>
-  </div>
+  <v-container>
+    <v-card class="mx-auto" max-width="400">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="@/assets/login_header.jpg"
+      >
+        <v-card-title>Register</v-card-title>
+      </v-img>
+
+      <v-card-text>
+        <v-form @submit.prevent="submit" ref="form">
+          <v-text-field
+            v-model="account.username"
+            name="username"
+            label="username"
+            id="username"
+            :rules="emailRules"
+            required
+          />
+
+          <v-text-field
+            v-model="account.password"
+            name="password"
+            label="password"
+            id="password"
+            :rules="passwordRules"
+            :append-icon="e1 ? 'visibility' : 'visibility_off'"
+            @click:append="e1 = !e1"
+            :type="e1 ? 'password' : 'text'"
+            counter
+          />
+
+          <v-layout row justify-space-between class="mt-4">
+            <v-btn flat color="primary">Cancel</v-btn>
+            <v-btn color="success" type="submit">Register</v-btn>
+          </v-layout>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "register",
+  name: "Register",
   data() {
     return {
       account: {
         username: "",
         password: "",
       },
+      emailRules: [(v) => !!v || "E-mail is required"],
+      passwordRules: [(v) => !!v || "Password is required"],
+      valid: false,
+      e1: true,
     };
   },
   methods: {
     submit() {
-      alert(JSON.stringify(this.account));
+      if (this.$refs.form.validate()) {
+        alert(JSON.stringify(this.account));
+      }
     },
   },
 };
