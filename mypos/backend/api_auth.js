0c06a0fdf590df6fcabf07488db07a671fcb9405
codemobiles <chaiyasit.t@gmail.com>
2020-11-24 15:38:38 +0700

    23

--- mypos/backend/api_auth.js
+++ mypos/backend/api_auth.js
@@ -2,26 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Users = require("./models/user_schema");
 const bcrypt = require("bcryptjs");
-
-
+const jwt = require("./jwt");
 
 router.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-    let doc = await Users.findOne({ username });
-  
-    if (doc) {
-      let isPasswordValid = await bcrypt.compare(password, doc.password);
-      if (isPasswordValid) {
-      
-        res.json({ result: "ok", token, message: "success" });
-      } else {
-        res.json({ result: "nok", message: "invalid password" });
-      }
-    } else {
-      res.json({ result: "nok", message: "invalid username" });
-    }
-  });
+  const { username, password } = req.body;
+  let doc = await Users.findOne({ username });
 
+  if (doc) {
+    let isPasswordValid = await bcrypt.compare(password, doc.password);
+    if (isPasswordValid) {
+      const payload = {
+        id: doc._id,
+        level: doc.level,
+        username: doc.username,
+      };
+
+      res.json({ result: "ok", message: "success" });
+    } else {
+      res.json({ result: "nok", message: "invalid password" });
+    }
+  } else {
+    res.json({ result: "nok", message: "invalid username" });
+  }
+});
 
 router.post("/register", async (req, res) => {
   try {
