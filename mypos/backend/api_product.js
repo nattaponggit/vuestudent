413f3848666d616d5fa36687d28818c2c6961b15
codemobiles <chaiyasit.t@gmail.com>
2020-11-24 15:10:35 +0700

    23

--- mypos/backend/api_product.js
+++ mypos/backend/api_product.js
@@ -1,23 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const {i1, i2} = require("./myinterceptors")
 // http://localhost:8081/api/v2/product
-
-const i1 = (req, res, next) => {
-  if (req.query.token1 == "1234") {
-    next();
-  } else {
-    res.end("Invalid token1");
-  }
-};
-
-const i2 = (req, res, next) => {
-  if (req.query.token2 == "4321") {
-    next();
-  } else {
-    res.end("Invalid token2");
-  }
-};
+// http://localhost:8081/api/v2/product?token1=1234&token2=4321
 
 router.get("/product", i1, i2, (req, res) => {
   res.json({ result: "product successfully" });
---
+++ mypos/backend/myinterceptors.js
@@ -0,0 +1,16 @@
+const i1 = (req, res, next) => {
+    if (req.query.token1 == "1234") {
+      next();
+    } else {
+      res.end("Invalid token1");
+    }
+  };
+  
+  const i2 = (req, res, next) => {
+    if (req.query.token2 == "4321") {
+      next();
+    } else {
+      res.end("Invalid token2");
+    }
+  };
+  
