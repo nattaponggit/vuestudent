80251e77edbee15a06e5712968f8452b7714cc39
codemobiles <chaiyasit.t@gmail.com>
2020-11-25 15:38:34 +0700

    comuted

--- mypos/src/main.js
+++ mypos/src/main.js
@@ -5,6 +5,7 @@ import vuetify from "./plugins/vuetify";
 import "material-design-icons-iconfont/dist/material-design-icons.css"; // https://iconify.design/icon-sets/mdi/
 import store from "./store";
 import Vue2Filters from "vue2-filters";
+import { imageUrl } from "@/services/constants";
 
 Vue.config.productionTip = false;
 Vue.use(Vue2Filters);
