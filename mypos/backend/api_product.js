a009287b6bc402e51f8e320fdbbc28967bb4aadc
codemobiles <chaiyasit.t@gmail.com>
2020-11-25 09:37:42 +0700

    23

--- mypos/backend/api_product.js
+++ mypos/backend/api_product.js
@@ -2,17 +2,19 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("./jwt");
 const Products = require("./models/product_schema");
+const formidable = require("formidable");
+const fs = require("fs-extra");
+
+
+
 // destructure
 const { i1, i2 } = require("./myinterceptors");
 // http://localhost:8081/api/v2/product
 // http://localhost:8081/api/v2/product?token1=1234&token2=4321
 
-router.get(
-  "/product",
-  /*jwt.verify,*/ async (req, res) => {
-    const doc = await Products.find({});
-    res.json(doc);
-  }
-);
+router.get("/product", jwt.verify, async (req, res) => {
+  const doc = await Products.find({}).sort({ created: -1 });;
+  res.json(doc);
+});
 
 module.exports = router;
