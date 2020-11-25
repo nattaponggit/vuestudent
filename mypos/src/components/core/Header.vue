6ca24d675de2d33c5dc4f58c7185e3accf8719b7
codemobiles <chaiyasit.t@gmail.com>
2020-11-25 13:51:33 +0700

    23

--- mypos/src/components/core/Header.vue
+++ mypos/src/components/core/Header.vue
@@ -1,11 +1,50 @@
-<template>
-  <div>Header {{ $store.state.count }}</div>
+<template lang="html">
+  <v-app-bar app color="#1c2835" dark>
+    <v-btn icon @click="$router.back()" v-if="isShowBackBtn">
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
+    <v-toolbar-title>CMPOS Workshop V{{ version }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <span>{{ $store.getters["username"] | capitalize }}</span>
+    <v-btn icon @click="onClickLogOff">
+      <v-icon>mdi-export</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
-<script>
-export default {
-  name: "Header",
-};
+<script lang="js">
+const sub_paths = [
+  "stock-create",
+  "stock-edit"]
+
+import api from "@/services/api";
+  export default  {
+    name: 'Header',
+    props: [],
+    mounted () {
+    },
+    computed: {
+      version(){
+        return process.env.VUE_APP_VERSION
+      },
+      isShowBackBtn(){
+        return this.$route.matched.some(({ name }) => {
+          let index = sub_paths.indexOf(name)
+          return index != -1
+          })
+      }
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+      onClickLogOff(){
+
+        this.$store.dispatch('doLogout')
+      }
+    }
+}
 </script>
 
-<style></style>
+<style scoped lang="scss"></style>
