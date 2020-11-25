04a4984c474bdef0a128ec540aee7daa521c1273
codemobiles <chaiyasit.t@gmail.com>
2020-11-25 14:41:07 +0700

    comuted

--- mypos/src/views/Stock.vue
+++ mypos/src/views/Stock.vue
@@ -2,7 +2,11 @@
   <v-container>
     <h1>Stock</h1>
     <div>
-    
+      <ul>
+        <li v-for="(item, index) in ['Angular', 'React', 'Vue']" :key="item">
+          {{ index }}. {{ item }}
+        </li>
+      </ul>
     </div>
   </v-container>
 </template>
