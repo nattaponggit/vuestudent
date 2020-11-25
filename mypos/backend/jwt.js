9a0aeb8ef2c08d15bcb0a41bd2f7aa8676689499
codemobiles <chaiyasit.t@gmail.com>
2020-11-25 10:12:55 +0700

    23

--- mypos/backend/jwt.js
+++ mypos/backend/jwt.js
@@ -1,53 +1,58 @@
-const fs = require('fs');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-var publicKEY = fs.readFileSync(path.join(__dirname + '/public.key'), 'utf8');
-var privateKEY = fs.readFileSync(path.join(__dirname + '/private.key'), 'utf8');
+const fs = require("fs");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+var publicKEY = fs.readFileSync(path.join(__dirname + "/public.key"), "utf8");
+var privateKEY = fs.readFileSync(path.join(__dirname + "/private.key"), "utf8");
 
-
-var i 	= 'CodeMobiles Ltd';    	// Issuer (Software organization who issues the token)
-var s 	= 'chaiyasit.t@gmail.com';	// Subject (intended user of the token)
-var a 	= 'http://codemobiles.com';	// Audience (Domain within which this token will live and function)
+var i = "CodeMobiles Ltd"; // Issuer (Software organization who issues the token)
+var s = "chaiyasit.t@gmail.com"; // Subject (intended user of the token)
+var a = "http://codemobiles.com"; // Audience (Domain within which this token will live and function)
 
 module.exports = {
-    sign : (payload)=>{
-         // Token signing options
-         var signOptions = {
-            issuer: 	i,
-            subject: 	s,
-            audience: 	a,
-            expiresIn: "30d",    // 30 days validity
-            algorithm: "RS256"
-        };
+  sign: (payload, expiresIn) => {
+    // Token signing options
+    var signOptions = {
+      issuer: i,
+      subject: s,
+      audience: a,
+      expiresIn: expiresIn, // 30 days validity
+      algorithm: "RS256",
+    };
+    return jwt.sign(payload, privateKEY, signOptions);
+  },
+  verify: (req, res, next) => {
+    //next();
+    var token = req.headers.authorization
+      ? req.headers.authorization.split(" ")[1]
+      : null;
+    if (!token)
+      return res
+        .status(403)
+        .json({ auth: false, message: "No token provided." });
 
-        // return jwt.sign(payload, 'god', signOptions);
-        return jwt.sign(payload, privateKEY, signOptions);
-    },
-    verify : (req, res, next)=>{ 
-        // next()
-        // return;
+    var verifyOptions = {
+      issuer: i,
+      subject: s,
+      audience: a,
+      expiresIn: "600",
+      algorithm: ["RS256"],
+    };
 
+    jwt.verify(token, publicKEY, verifyOptions, function (err, decoded) {
+      if (err) {
+        if (err.name == "TokenExpiredError") {
+          return res
+            .status(401)
+            .json({ auth: false, message: "token expired" });
+        } else {
+          return res.status(500).json({ auth: false, message: err });
+        }
+      }
 
-        var token = req.headers['x-access-token'];
-        if (!token)
-        return res.status(403).json({ result: "nok", message: 'No token provided.' });
-
-        var verifyOptions = {
-            issuer: 	i,
-            subject: 	s,
-            audience: 	a,
-            expiresIn: 	"12h",
-            algorithm: 	["RS256"]
-        };      
-
-        jwt.verify(token, publicKEY, verifyOptions, function(err, decoded) {
-            console.log(JSON.stringify(decoded));
-            if (err)
-            return res.status(500).json({ result: "nok", message: 'Failed to authenticate token.' });
-            // if everything good, save to request for use in other routes
-            req.userId = decoded.id;
-            req.userLevel = decoded.level;
-            next();
-        });
-    }
-}
+      // if everything good, save to request for use in other routes
+      req.userId = decoded.id;
+      req.userLevel = decoded.level;
+      next();
+    });
+  },
+};
