8b541e5872e970adee6a8634d960a8529770aa8b
codemobiles <chaiyasit.t@gmail.com>
2020-11-25 09:46:24 +0700

    23

--- mypos/backend/api_product.js
+++ mypos/backend/api_product.js
@@ -6,6 +6,21 @@ const formidable = require("formidable");
 const fs = require("fs-extra");
 
 
+uploadImage = async (files, doc) => {
+  if (files.image != null) {
+    var fileExtention = files.image.name.split(".")[1];
+    doc.image = `${doc.product_id}.${fileExtention}`;
+    var oldpath = files.image.path;
+    var newpath = __dirname + "/uploaded/images/" + doc.image;
+    if (fs.exists(newpath)) {
+      await fs.remove(newpath);
+    }
+    await fs.move(oldpath, newpath);
+    await Products.findOneAndUpdate({ product_id: doc.product_id }, doc);
+  }
+};
+
+
 router.post("/product", (req, res)=>{
   const form = new formidable.IncomingForm()
   form.parse(req, (err, fields, files)=>{
