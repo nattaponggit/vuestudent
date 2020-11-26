c4631c1b8cd7cc17922bb9a4ffda75664f6f29a0
codemobiles <chaiyasit.t@gmail.com>
2020-11-26 11:14:54 +0700

    1234

--- mypos/src/views/Payment.vue
+++ mypos/src/views/Payment.vue
@@ -1,10 +1,18 @@
 <template>
-  <div><v-btn block color="success" dark>Payment</v-btn></div>
+  <div><v-btn @click="onSubmit" block color="success" dark>Payment</v-btn></div>
 </template>
 
 <script>
 export default {
   name: "payment",
+  methods: {
+    onSubmit() {
+      this.$emit("onResult", {
+        status: "ok",
+        change: 9,
+      });
+    },
+  },
 };
 </script>
 
