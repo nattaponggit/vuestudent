6aed11aa2b132890d2612dcec3c0eb5863a04de4
codemobiles <chaiyasit.t@gmail.com>
2020-11-26 11:28:20 +0700

    1234

--- mypos/backend/api_transaction.js
+++ mypos/backend/api_transaction.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Trans = require("./models/trans_schema");
 
 router.get("/transaction", (req, res) => {
   res.json({ result: "transaction successfully" });
+});
+
+router.post("/transaction", async (req, res) => {
+  console.log(JSON.stringify(req.body));
+  const doc = await Trans.create(req.body)
+  res.json({ result: "ok", doc });
 });
 
 module.exports = router;
