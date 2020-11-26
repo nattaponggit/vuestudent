0f9c88f4a7bdfebdd89ce7be09925f96b9ca742a
codemobiles <chaiyasit.t@gmail.com>
2020-11-26 13:10:25 +0700

    1234

--- mypos/src/views/Transaction.vue
+++ mypos/src/views/Transaction.vue
@@ -1,10 +1,26 @@
 <template>
-  <div><h1>Transaction</h1></div>
+  <v-data-table
+    :headers="headers"
+    :items="data"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
 </template>
 
 <script>
+import api from "@/services/api";
 export default {
   name: "transaction",
+  async mounted() {
+    const result = await api.getTransactions();
+    this.data = result.data;
+  },
+  data() {
+    return {
+      headers: [{ text: "Id", value: "transaction_id" }],
+      data: [],
+    };
+  },
 };
 </script>
 
