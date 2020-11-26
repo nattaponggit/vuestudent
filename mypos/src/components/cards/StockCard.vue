5118987ee8c2421d0f630b33db9d8c3b2f1b85f4
codemobiles <chaiyasit.t@gmail.com>
2020-11-26 09:22:44 +0700

    1234

--- mypos/src/components/cards/StockCard.vue
+++ mypos/src/components/cards/StockCard.vue
@@ -2,12 +2,12 @@
   <v-card>
     <v-list-item two-line>
       <v-list-item-content>
-        <v-list-item-title>xxx</v-list-item-title>
-        <v-list-item-subtitle>yyyy</v-list-item-subtitle>
+        <v-list-item-title>{{title}}</v-list-item-title>
+        <v-list-item-subtitle>{{subtitle}}</v-list-item-subtitle>
       </v-list-item-content>
 
       <v-list-item-avatar>
-        <v-icon>mdi-cart-outline</v-icon>
+        <v-icon>{{avatar_ic}}</v-icon>
       </v-list-item-avatar>
     </v-list-item>
   </v-card>
