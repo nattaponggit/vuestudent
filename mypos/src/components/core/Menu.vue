7e8b26895ee80320df22db59eb91f17cee7326dc
codemobiles <chaiyasit.t@gmail.com>
2020-11-26 10:46:07 +0700

    1234

--- mypos/src/components/core/Menu.vue
+++ mypos/src/components/core/Menu.vue
@@ -1,11 +1,85 @@
-<template>
-  <div>Menu {{ $store.state.count }}</div>
+<template lang="html">
+  <v-navigation-drawer
+    app
+    permanent
+    absolute
+    dark
+    src="@/assets/background_menu.jpg"
+  >
+    <router-link to="/" exact>
+      <img src="@/assets/vue_display.jpg" alt="" width="100%" />
+    </router-link>
+
+    <v-list shaped>
+      <v-subheader>MENUS</v-subheader>
+      <v-list-item-group v-model="selectedMenu" mandatory color="primary">
+        <v-list-item
+          class="tile"
+          v-for="([icon, text, route], i) in menus"
+          :key="i"
+          link
+          @click="onClickMenu(route)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
-<script>
-export default {
-  name: "Menu",
-};
+<script lang="js">
+
+  export default  {
+    name: 'Menu',
+    props: [],
+    mounted () {
+      this.selectedMenu = this.menus.findIndex(menu=> menu[2] == this.$route.path)
+    },
+    data () {
+      return {
+        selectedMenu:0,
+        menus: [
+        ['mdi-apps-box', 'Stock', '/stock'],
+        ['mdi-cart-outline', 'Shop', '/shop'],
+        ['mdi-chart-areaspline', 'Report', '/report'],
+        ['mdi-folder-multiple-outline', 'Transaction', '/transaction'],
+
+      ],
+      }
+    },
+    methods: {
+      onClickMenu(link){
+        this.$router.push(link).catch(err => {})
+      }
+    },
+    watch:{
+     $route(to, from) {
+       this.selectedMenu = this.menus.findIndex(menu=> menu[2] == to.path)
+      }
+    },
+    computed: {
+
+    }
+}
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.menu {
+}
+
+.tile {
+  color: #fff;
+}
+.tile:hover {
+  background: green;
+}
+.tile:active {
+  background: #05ab71;
+}
+</style>
