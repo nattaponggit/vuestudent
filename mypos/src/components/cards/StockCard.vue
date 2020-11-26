b25c7967bd236b2e5bf360eb7c6953b1339668b6
codemobiles <chaiyasit.t@gmail.com>
2020-11-26 09:35:00 +0700

    1234

--- mypos/src/components/cards/StockCard.vue
+++ mypos/src/components/cards/StockCard.vue
@@ -7,7 +7,7 @@
       </v-list-item-content>
 
       <v-list-item-avatar size="80" :color="avatar_bg">
-        <v-slot></v-slot>
+        <slot name="avatar"></slot>
       </v-list-item-avatar>
     </v-list-item>
   </v-card>
