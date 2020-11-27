c5af37d70b26736c6fc0ed97b3a917b4755f3a04
codemobiles <chaiyasit.t@gmail.com>
2020-11-27 13:29:24 +0700

    1234

--- mymypos/src/App.vue
+++ mymypos/src/App.vue
@@ -2,13 +2,21 @@
   <v-app>
     <v-container>
       <v-content>
-        <v-card>
+        <v-card max-width="300" class="pa-5">
           <v-form>
             <v-text-field
               name="username"
               label="username"
               id="username"
             ></v-text-field>
+
+            <v-text-field
+              name="password"
+              label="password"
+              id="password"
+            ></v-text-field>
+
+            <v-btn class="mt-5" block color="primary" dark>Register</v-btn>
           </v-form>
         </v-card>
       </v-content>
