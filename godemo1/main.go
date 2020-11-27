87fec4be89ab71bf378ea6691cd0e27f74c91e2f
codemobiles <chaiyasit.t@gmail.com>
2020-11-27 09:48:43 +0700

    1234

--- godemo1/main.go
+++ godemo1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
 	fmt.Println("Hello go")
@@ -13,8 +14,27 @@ func main() {
 	// Implicit Declartion
 	t1 := "codemobiles"
 	t2 := 10
-	t3 := false
+	// t3 := false
 
 	fmt.Println(tmp1, tmp2, tmp3)
-	fmt.Printf("%s,%d,%v", t1, t2, t3)
+	var msg = fmt.Sprintf("%s,%d", t1, t2)
+	fn1(msg)
+	fmt.Println(fn2("Lek"))
+
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func fn1(msg string) {
+	fmt.Println("Msg : " + msg)
+}
+
+func fn2(msg string) string {
+	return msg + " : Yes"
 }
