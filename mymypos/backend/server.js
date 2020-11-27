29d799fd9a5043dc4d9d777e3dd82d7cb30b92a7
codemobiles <chaiyasit.t@gmail.com>
2020-11-27 13:39:34 +0700

    1234

--- mymypos/backend/server.js
+++ mymypos/backend/server.js
@@ -4,17 +4,21 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const Users = require("./user_schema")
-require("./db")
+const Users = require("./user_schema");
+require("./db");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/register", async (req, res)=>{
-    const doc = await Users.create(req.body)
-    res.json(doc)
-})
+app.post("/register", async (req, res) => {
+  try {
+    const doc = await Users.create(req.body);
+    res.json({status: "ok", message:"Register successfully"});
+  } catch (e) {
+    res.json({status: "nok", message: JSON.stringify(e)})
+  }
+});
 
 app.listen(8081, () => {
   console.log("server is running.");
